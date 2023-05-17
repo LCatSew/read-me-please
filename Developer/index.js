ec1b8131db89c6955636ca943977a06056587dc4
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-05-16 22:13:24 -0500

    generateMarkdown.js has been updated with a function/const to generate the readme file based on inpiut from inquirer. index.js still needs updating with the prompts and additional fiunctions need to be added to the generatemarkdown file for licenses and badges but a readme file is being generated

--- Developer/index.js
+++ Developer/index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+//const fs = require('fs');
+
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const { writeFile } = require('fs').promises;
+
+console.log(generateMarkdown);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Using full sentence structure, what was your motivation?',
+            name: 'motivation',
+        },
+        {
+            type: 'input',
+            message: 'Using full sentence structure, why did you build this project?',
+            name: 'build',
+        },
+        {
+            type: 'input',
+            message: 'Using full sentence structure, what problem does it solve?',
+            name: 'problem',
+        },
+        {
+            type: 'input',
+            message: 'Using full sentence structure, what did you learn?',
+            name: 'learn',
+        },
+        {
+        type: 'confirm',
+        message: 'Are you using a license?',
+        name: 'ruLicnese'
+        },
+    ])
+};
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+// }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+    .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully created README.md'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
